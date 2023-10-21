81aeb7d20db0e24163a1998a4cddec4ec5f852cc
karthik raj <123324653+Karthik19200@users.noreply.github.com>
2023-10-21 19:21:33 +0530

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,7 +11,7 @@ export class ApiService {
   baseUrl = 'http://localhost:3000/contacts'
 
   //dependency injection 
-  constructor(private http: HttpClient) { } // hover for quick fix (then it will import)
+  constructor(private http: HttpClient) { } 
 
   //function for get all contacts
   getAllContact(): Observable<MyContact> {
